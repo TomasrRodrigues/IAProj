159181a4643138531f8570a39935e4cc70e5099f
Pedro Oliveira <up202000149@up.pt>
2025-03-14 03:46:32 +0000

    rudimentary movement

--- game.py
+++ game.py
@@ -1,12 +1,24 @@
 # Example file showing a circle moving on screen
 import pygame
+import re
 
-width = 100
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+font = pygame.font.Font(None, 32)
+input_box = pygame.Rect(100, 100, 140, 32)
+clock = pygame.time.Clock()
+running = True
+dt = 0
+text = ''
+current_player = "black"
 
-tiles = {(1,1):{"color":"grey","pos":(-3*width,0)},              (1,2):{"color":"darkblue","pos":(9/4*width,-width/2)}, (1,3):{"color":"darkblue","pos":(3/2*width,-width)},(1,4):{"color":"darkblue","pos":(3/4*width,-3/2*width)},(1,5):{"color":"grey","pos":(0,-2*width)},
-         (2,1):{"color":"darkblue","pos":(-9/4*width,width/2)},  (2,2):{"color":"white","pos":(-3/2*width,0)},          (2,3):{"color":"white","pos":(3/4*width,-width/2)}, (2,4):{"color":"white","pos":(0,-width)},               (2,5):{"color":"darkblue","pos":(-3/4*width,-3/2*width)},
-         (3,1):{"color":"darkblue","pos":(-3/2*width,width)},    (3,2):{"color":"white","pos":(-3/4*width,width/2)},    (3,3):{"color":"grey","pos":(0,0)},                 (3,4):{"color":"white","pos":(-3/4*width,-width/2)},    (3,5):{"color":"darkblue","pos":(-3/2*width,-width)},
-         (4,1):{"color":"darkblue","pos":(-3/4*width,3/2*width)},(4,2):{"color":"white","pos":(0,width)},               (4,3):{"color":"white","pos":(3/4*width,width/2)},  (4,4):{"color":"white","pos":(3/2*width,0)},            (4,5):{"color":"darkblue","pos":(-9/4*width,-width/2)},
+width = screen.get_height()/8
+
+tiles = {(1,1):{"color":"grey","pos":(-3*width,0)},              (1,2):{"color":"darkblue","pos":(-9/4*width,-width/2)}, (1,3):{"color":"darkblue","pos":(-3/2*width,-width)},(1,4):{"color":"darkblue","pos":(-3/4*width,-3/2*width)},(1,5):{"color":"grey","pos":(0,-2*width)},
+         (2,1):{"color":"darkblue","pos":(-9/4*width,width/2)},  (2,2):{"color":"white","pos":(-3/2*width,0)},          (2,3):{"color":"white","pos":(-3/4*width,-width/2)}, (2,4):{"color":"white","pos":(0,-width)},               (2,5):{"color":"darkblue","pos":(3/4*width,-3/2*width)},
+         (3,1):{"color":"darkblue","pos":(-3/2*width,width)},    (3,2):{"color":"white","pos":(-3/4*width,width/2)},    (3,3):{"color":"grey","pos":(0,0)},                 (3,4):{"color":"white","pos":(3/4*width,-width/2)},    (3,5):{"color":"darkblue","pos":(3/2*width,-width)},
+         (4,1):{"color":"darkblue","pos":(-3/4*width,3/2*width)},(4,2):{"color":"white","pos":(0,width)},               (4,3):{"color":"white","pos":(3/4*width,width/2)},  (4,4):{"color":"white","pos":(3/2*width,0)},            (4,5):{"color":"darkblue","pos":(9/4*width,-width/2)},
          (5,1):{"color":"grey","pos":(0,2*width)},               (5,2):{"color":"darkblue","pos":(3/4*width,3/2*width)},(5,3):{"color":"darkblue","pos":(3/2*width,width)}, (5,4):{"color":"darkblue","pos":(9/4*width,width/2)},   (5,5):{"color":"grey","pos":(3*width,0)}}
 
 
@@ -55,15 +67,44 @@ def drawPieces(l):
         pygame.draw.circle(screen, "black", pos, width/4, 1)
     return
 
-# pygame setup
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
-dt = 0
+def parseInput(text):
+    place = re.search("^[(][1-5],[1-5][)]$", text)
+    move = re.search("^[(][1-5],[1-5][)][-][(][1-5],[1-5][)]$", text)
+    if place: 
+        parse = (place.string.strip("()").split(","))
+        print((int(parse[0]),int(parse[1])))
+        if movePiece((-1,-1), (int(parse[0]),int(parse[1]))): return 1
+        return 0
+    elif move:
+        parse = move.string.split("-")
+        ip = parse[0].strip("()").split(",")
+        fp = parse[1].strip("()").split(",")
+        print((int(ip[0]),int(ip[1])),(int(fp[0]),int(fp[1])))
+        if movePiece((int(ip[0]),int(ip[1])),(int(fp[0]),int(fp[1]))): return 1
+        return 0
+    else:
+        print("Invalid input")
+        return 1
 
-#tiles ((x,y), colour)
+def movePiece(ip,fp):
+    valid = True
+    
 
+    for piece in pieces:
+        if piece[0] == fp: 
+            valid = False
+            print("Invalid move")
+            return 1
+
+    if valid:
+        for piece in pieces:
+            if piece[0] == ip and piece[1] == current_player:
+                n = pieces.index(piece)
+                pieces[n] = (fp, piece[1])
+                
+                return 0
+    print("Invalid move")
+    return 1
 
 center_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
@@ -73,17 +114,30 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+            if event.key == pygame.K_RETURN:
+                if not parseInput(text):
+                    if current_player == "black": current_player = "white"
+                    else: current_player = "black"
+                    print(current_player)
+                text = ''
+            elif event.key == pygame.K_BACKSPACE:
+                text = text[:-1]
+            else:
+                text += event.unicode
 
+    #print(pieces)
     screen.fill("lightgoldenrod")
     drawBoard(tiles)
     drawPieces(pieces)
-    if pygame.mouse.get_pressed()[0]:
-        click = pygame.mouse.get_pos()
-        pygame.draw.circle(screen, "black", click, 1/4 * width)
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_ESCAPE]:
-        running = False
+    txt_surface = font.render(text, True, "black")
+    width_txt = max(200, txt_surface.get_width()+10)
+    input_box.w = width_txt
+    screen.blit(txt_surface, (input_box.x+5, input_box.y+5))
+    pygame.draw.rect(screen, "white", input_box, 2)     
 
     # flip() the display to put your work on screen
     pygame.display.flip()
